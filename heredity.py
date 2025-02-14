a7be31a80907bc5aa41af652b461da97af668460
sarganio <sarganio@post.bgu.ac.il>
2025-02-14 10:37:41 +0200

    pagerank.py:finished joint_probability func

--- heredity.py
+++ heredity.py
@@ -139,7 +139,48 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         * everyone in set `have_trait` has the trait, and
         * everyone not in set` have_trait` does not have the trait.
     """
-    raise NotImplementedError
+    ans = 1
+    for person in people:
+        # check if this person has known parents, if he/she doesn't take probability from PROBS
+        if person['father'] == None:
+            ans *= num_of_genes(person, one_gene, two_genes)
+            ans *= PROBS["trait"][person in have_trait]
+        # else the person does have known parents - take mutation into account
+        else:
+            # probabiliy gene was inheirated from parent given parent has [index] genes
+            inheriateFromParentProb = []
+            # parent has 0 genes but mutation accured
+            inheriateFromParentProb[0] = PROBS["mutation"]
+            # parent has 1 genes and that genes was inherited or the other was passed on and mutation accured
+            inheriateFromParentProb[1] = 0.5 + 0.5*PROBS["mutation"]
+            # parent has 2 genes no mutation was accured
+            inheriateFromParentProb[2] = 1 - PROBS["mutation"]
+            fatherNumOfGenes = num_of_genes(people[person['father']])
+            motherNumOfGenes = num_of_genes(people[person['mother']])
+            # 1 gene - person got a gene from father or mother but not both
+            if person in one_gene:
+                ans *= inheriateFromParentProb[fatherNumOfGenes] * (1 - inheriateFromParentProb[motherNumOfGenes]) + (1 - inheriateFromParentProb[fatherNumOfGenes]) * inheriateFromParentProb[motherNumOfGenes]
+            # 2 genes - person got a both a gene from father and mother
+            elif person in two_genes:
+                ans *= inheriateFromParentProb[fatherNumOfGenes] * inheriateFromParentProb[motherNumOfGenes]
+            # 0 genes - person got no gene from father and mother
+            else:
+                # no inheritace from father and mother
+                ans *= (1 - inheriateFromParentProb[fatherNumOfGenes]) * (1 - inheriateFromParentProb[motherNumOfGenes])
+    return ans
+
+
+def num_of_genes(person, one_gene, two_genes):
+    if person in one_gene:
+        return PROBS["gene"][1]
+    if person in two_genes:
+        return PROBS["gene"][2]
+    return PROBS["gene"][0] 
+
+
+def calculate_inheritance_given_genes_num(person, people, genesNum):
+    if genesNum == 0:
+        print(1)
 
 
 def update(probabilities, one_gene, two_genes, have_trait, p):
